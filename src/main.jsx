ef7391a24dbec537149ece0a218c826b8e8e7945
orji Dominion <orjidominion32@gmail.com>
2025-06-10 18:20:50 +0100

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,6 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-{/* <h1>Hello AppContent</h1> */}
 <App />
-{/* <h2>Chimooo</h2> */}
 </>);
 
